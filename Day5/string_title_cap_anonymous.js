05966e15b4eec816e285127ae72b94f10a47a818
ponrajvishunu <ponrajsm@gmail.com>
2022-07-31 11:49:26 +0530

    da5_tasks

---
+++ Day5/string_title_cap_anonymous.js
@@ -0,0 +1,21 @@
+let string = "i am guvian";
+
+let a = function(str) {
+    let val = str.toLowerCase().split(" ");
+    //console.log(val);
+    for(let i = 0; i< val.length; i++){
+        //console.log(val[i].slice(1));
+        val[i] = val[i][0].toUpperCase() + val[i].slice(1);
+    }
+    //console.log(val.join(" "));
+    val.join(" ");
+    let res = "";
+
+    for(let i = 0;i < val.length-1;i++){
+        res += val[i]+" ";
+    }
+    res += val[val.length-1];
+    return res;
+}
+
+console.log(a(string));
