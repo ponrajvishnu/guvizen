e8198842042b5a5a47e5168acbfee97126f2ea74
ponrajvishunu <ponrajsm@gmail.com>
2022-08-29 15:21:19 +0530

    Day6 Task

---
+++ Day6/script.js
@@ -0,0 +1,17 @@
+// alert("I'm invoked!");
+
+// alert('Hello') // this line is not having semicolon
+// alert(`Wor
+//  ld`)
+// alert(3 +
+// 1
+// + 2); // this is multiple line code and its working
+
+
+let admin = 9, fname = 10.5;
+fname = "Guvi";
+lname = "Geek";
+admin = fname + lname;
+
+//alert(admin);
+alert(`Hello ${admin}`);
