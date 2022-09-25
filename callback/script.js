0ba89395f17a209240415d8fd5d2a41aa2b47139
ponrajvishnu <ponrajsm@gmail.com>
2022-09-25 17:59:52 +0530

    callback task

---
+++ callback/script.js
@@ -0,0 +1,37 @@
+let callfun = () => {
+    setTimeout(() => {
+        document.querySelector("#text-content").innerText = 10;
+        return setTimeout(() => {
+            document.querySelector("#text-content").innerText = 9;
+            return setTimeout(() => {
+                document.querySelector("#text-content").innerText = 8;
+                return setTimeout(() => {
+                    document.querySelector("#text-content").innerText = 7;
+                    return setTimeout(() => {
+                        document.querySelector("#text-content").innerText = 6;
+                        return setTimeout(() => {
+                            document.querySelector("#text-content").innerText = 5;
+                            return setTimeout(() => {
+                                document.querySelector("#text-content").innerText = 4;
+                                return setTimeout(() => {
+                                    document.querySelector("#text-content").innerText = 3;
+                                    return setTimeout(() => {
+                                        document.querySelector("#text-content").innerText = 2;
+                                        return setTimeout(() => {
+                                            document.querySelector("#text-content").innerText = 1;
+                                            return setTimeout(() => {
+                                                document.querySelector("#text-content").innerText = 'Happy Independence Day';
+                                            },1000)
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+}
+
+callfun();
